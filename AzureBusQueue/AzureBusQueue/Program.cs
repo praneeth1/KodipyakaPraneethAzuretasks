921ee6ff3e710883407d52af25ff4be3e4226da6
Praneeth Kodipyaka <praneeth.goud@yash.com>
2020-05-28 16:05:38 +0530

    Import azurebus project

---
+++ AzureBusQueue/AzureBusQueue/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace AzureBusQueue
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "";
+        const string QueueName = "";
+        static IQueueClient queueClient;
+        public static async Task Main()
+        {
+            const int numberofmessages = 10;
+            queueClient = new QueueClient(ServiceBusConnectionString,QueueName);
+            Console.WriteLine("=======================================");
+            Console.WriteLine("Press enter KEY after exit to send all meassages ");
+            Console.WriteLine("=======================================");
+            //send mesages
+            await SendMessageAynsc(numberofmessages);
+            Console.ReadKey();
+            await queueClient.CloseAsync();
+
+        }
+        static async Task SendMessageAynsc(int numberofmessagestosend)
+        {
+            try
+            {
+                for (var i = 0; i < numberofmessagestosend; i++)
+                {
+                    string messgaeBody = $"messgae{i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messgaeBody));
+                    Console.WriteLine($"sending message:{messgaeBody}");
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
